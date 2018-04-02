8eb2ea4b3caeff7fd0d0dd9b08037993f5096777
Aldiantoro Nugroho <kriwil@gmail.com>
2018-04-02 09:48:37 +0700

    initial view

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+from github import Github
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('main.html')
